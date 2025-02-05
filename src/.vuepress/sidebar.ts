a27eb3a6ffd46f60f7ad727491958833025b9cbf
AlanShelby <18866545703@163.com>
2025-02-05 18:02:17 +0800

    修改文件目录

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,14 +1,26 @@
-import { sidebar } from "vuepress-theme-hope";
+import {sidebar} from "vuepress-theme-hope";
 
 export default sidebar({
-  "/": [
-    "",
-    "portfolio",
-    {
-      text: "Linux相关",
-      icon: "book",
-      prefix: "Linux/",
-      children: "structure",
-    },
-  ],
+    "/": [
+        "",
+        "portfolio",
+        {
+            text: "Linux相关",
+            icon: "book",
+            prefix: "Linux/",
+            children: [
+                {
+                    text: "Linux基础",
+                    prefix: "Linux基础/",
+                    children: [
+                        "Linux 系统文件.md",
+                        "Linux 命令集合.md",
+                        "Vim 文本编辑器.md",
+                        "Linux 用户管理.md",
+                        "Linux 文件权限管理.md"
+                    ],
+                }
+            ],
+        },
+    ],
 });
